7ffc258cbc0a98f8f187f8f4055b37aa7bc4c8c0
Ahmet Topal <37218739+ahmettopal@users.noreply.github.com>
2022-07-26 10:51:38 +0300

    add exception filters

---
+++ src/extensions/filters/http-exception.filter.ts
@@ -0,0 +1,21 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const exceptionMessage = exception.getResponse() as object
+
+    response
+      .status(status)
+      .json({
+        ...exceptionMessage,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+  }
+}
